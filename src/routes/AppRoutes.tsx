c4e38dde7229f1b83b3dee6388ebf15fd33baf47
AdedayoDev <johnadedayo1@gmail.com>
2026-08-13 16:58:05 +0100

    Policy and get help page refreshing function fixed

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,16 +1,15 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import LandingPage from "../pages/LandingPage";
-import {GetHelp} from "../pages/GetHelp/GetHelp";
+import { GetHelp } from "../pages/GetHelp/GetHelp";
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../sections/Footer";
 import WhatsAppFloat from "../components/WhatsAppFloat";
 import { Policy } from "../pages/Policy/Policy";
-  
 
 function AppRoutes() {
   return (
-    <BrowserRouter>
-    <Navbar />
+    <Router>
+      <Navbar />
       <Routes>
         <Route path='/' element={<LandingPage />} />
         <Route path='/get-help' element={<GetHelp />} />
@@ -18,7 +17,7 @@ function AppRoutes() {
       </Routes>
       <Footer />
       <WhatsAppFloat />
-    </BrowserRouter>
+    </Router>
   );
 }
 
